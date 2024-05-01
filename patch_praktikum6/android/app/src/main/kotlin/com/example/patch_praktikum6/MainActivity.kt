6112ff13d9bbc9b391f5aca75b30f4e16b389f56
unknown <nadiyahsyaidatus.sah@gmail.com>
2024-05-01 21:11:33 +0700

    Pesan commit

---
+++ patch_praktikum6/android/app/src/main/kotlin/com/example/patch_praktikum6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.patch_praktikum6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ put_praktikum6/android/app/src/main/kotlin/com/example/put_praktikum6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.put_praktikum6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
